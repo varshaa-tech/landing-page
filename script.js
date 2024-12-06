555341b1c6436694bde1506b68f0945f83f049b8
Varsha <varsha040219@gmail.com>
2024-12-06 20:41:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const color3=document.getElementById('bg');
+color3.addEventListener('click',() =>{
+    document.getElementById('background').style.backgroundColor="black";
+    document.getElementById('background').style.color="white";
+    document.getElementById('a-color').style.color="white";
+    document.getElementById('a-color2').style.color="white";
+    document.getElementById('a-color3').style.color="white";
+    document.getElementById('a-color4').style.color="white";
+    document.getElementById('home').style.backgroundImage="linear-gradient(rgba(250, 250, 252, 0.413),rgba(250, 250, 252, 0.413))";
+    document.getElementById('collection').style.backgroundImage="linear-gradient(rgba(250, 250, 252, 0.413),rgba(250, 250, 252, 0.413))";
+    document.getElementsBycl('header1').style.color="white";
+});
